5e22d28dacebe63267ccaeaf90081f55c931e54e
Capgemini <capgemini@none.com>
2014-03-14 07:58:16 +0000

    v2 script sql short url table

--- _Database/Tables/dbo.ShortUrl.Table.sql
+++ _Database/Tables/dbo.ShortUrl.Table.sql
@@ -1,23 +1,29 @@
 USE [U3GPPDB]
 GO
 
-/****** Object:  Table [dbo].[ShortUrl]    Script Date: 03/14/2014 08:52:49 ******/
+/****** Object:  Table [dbo].[ShortUrl]    Script Date: 03/14/2014 08:57:35 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
+SET ANSI_PADDING ON
+GO
+
 CREATE TABLE [dbo].[ShortUrl](
 	[Pk_Id] [int] NOT NULL,
-	[Token] [nvarchar](10) NOT NULL,
-	[Url] [nvarchar](200) NOT NULL,
+	[Token] [varchar](50) NOT NULL,
+	[Url] [varchar](200) NOT NULL,
  CONSTRAINT [PK_ShortUrl] PRIMARY KEY CLUSTERED 
 (
 	[Pk_Id] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
+GO
+
+SET ANSI_PADDING OFF
 GO
 
 
