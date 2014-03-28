22c86978328b35218666ed4c4a2c352b15a1d39d
Capgemini <capgemini@none.com>
2014-03-28 17:30:55 +0000

    Updating the meetings view to encompass possible SMG and GSM meetings.

--- Database/dbo/Views/View_Meetings.sql
+++ Database/dbo/Views/View_Meetings.sql
@@ -1,14 +1,15 @@
-﻿CREATE VIEW [dbo].[View_Meetings]
+﻿
+CREATE VIEW [dbo].[View_Meetings]
 AS
 SELECT     MTG_ID, MTG_CATEG_CODE, MTG_TYPE_CODE, MTGS_CODE, MTG_REF, START_DATE, END_DATE, MTG_YEAR, MTG_SEQNO, MTG_TITLE, OWNERSHIP_LEVEL, INVITATION_DATE, DEADLINE_DATE, DURATION, INVITED_NUMB, ATTENDANT_NUMB, LOCAL_FLG, ORGA_ID, 
                       LOC_ADDRESS, LOC_ZIP, LOC_CITY, LOC_CTY_CODE, MTGREF_MASK, START_REGISTRATION_DATE, LOCATION_DETAILS_URL, TB_ID, 
                       Creation_Date, csn.ShortName, csn.ShortName+'-'+CONVERT(varchar(20),MTG_SEQNO) as MtgShortRef
-FROM         [$(DSDB)].dbo.MEETINGS AS MEETINGS_1
+FROM         [DSDB].dbo.MEETINGS AS MEETINGS_1
 INNER JOIN dbo.Enum_CommunitiesShortName csn ON csn.Fk_TbId = TB_ID
 WHERE     (TB_ID IN
                           (SELECT     TB_ID
-                            FROM          [$(DSDB)].dbo.TAB_TB
-                            WHERE      (TB_KEY1 = '3GPP')))
+                            FROM          [DSDB].dbo.TAB_TB
+                            WHERE      (TB_KEY1 = '3GPP') OR (TB_KEY1='SMG') OR (TB_KEY1='GSM')))
 GO
 
 EXECUTE sp_addextendedproperty @name = N'MS_DiagramPane1', @value = N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
