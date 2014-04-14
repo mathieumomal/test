9f13dfb516156e7d8556751cda858a9c94a4df20
Capgemini <capgemini@none.com>
2014-04-14 09:55:21 +0000

    Forgetting about RapporteurControl

---
+++ Modules/Controls/RapporteurControl.ascx.designer.cs
@@ -0,0 +1,60 @@
+﻿//------------------------------------------------------------------------------
+// <généré automatiquement>
+//     Ce code a été généré par un outil.
+//
+//     Les modifications apportées à ce fichier peuvent provoquer un comportement incorrect et seront perdues si
+//     le code est régénéré.
+// </généré automatiquement>
+//------------------------------------------------------------------------------
+
+namespace Etsi.Ultimate.Controls {
+    
+    
+    public partial class RapporteurControl {
+        
+        /// <summary>
+        /// Contrôle lblLegendRapporteur.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblLegendRapporteur;
+        
+        /// <summary>
+        /// Contrôle rdGridRapporteurs.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::Telerik.Web.UI.RadGrid rdGridRapporteurs;
+        
+        /// <summary>
+        /// Contrôle lblAddRapporteur.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAddRapporteur;
+        
+        /// <summary>
+        /// Contrôle txtAddRemark.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAddRemark;
+        
+        /// <summary>
+        /// Contrôle btnAddRapporteur.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAddRapporteur;
+    }
+}
