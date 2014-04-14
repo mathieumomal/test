a57f9c16b38d991defc81a6d5e2159ac77c9fcdf
Capgemini <capgemini@none.com>
2014-04-14 10:52:17 +0000

    Entity Framework - Status Flag Implementation

---
+++ Common/DomainClasses/Enum_EntityStatus.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Etsi.Ultimate.DomainClasses
+{
+    /// <summary>
+    /// Status flags to identify the object state
+    /// New - New Entity
+    /// Modified - Entity Modified
+    /// Deleted - Entity Deleted
+    /// Unchanged - Entity Unchanged
+    /// </summary>
+    public enum Enum_EntityStatus
+    {
+        Unchanged = 0, //Default Status
+        New,
+        Modified,
+        Deleted
+    }
+}
