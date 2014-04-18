4868a6e80e8c9d91730fb128ad7216801bf578d5
Capgemini <capgemini@none.com>
2014-04-18 15:04:25 +0000

    View Communities - Unit Tests

---
+++ Common/Tests/Business/CommunityManagerTest.cs
@@ -0,0 +1,57 @@
+﻿using Etsi.Ultimate.Business;
+using Etsi.Ultimate.DataAccess;
+using Etsi.Ultimate.DomainClasses;
+using Etsi.Ultimate.Repositories;
+using Etsi.Ultimate.Tests.FakeRepositories;
+using Etsi.Ultimate.Utils;
+using Microsoft.Practices.Unity;
+using NUnit.Framework;
+using Rhino.Mocks;
+using System.Data.Entity;
+
+namespace Etsi.Ultimate.Tests.Business
+{
+    [TestFixture]
+    public class CommunityManagerTest : BaseTest
+    {
+        private static string cacheKey = "ULT_COMMUNITY_MANAGER_ALL";
+
+        [Test]
+        public void CommunityManager_GetAll()
+        {
+            CacheManager.Clear(cacheKey);
+            CommunityFakeRepository fakeRepo = new CommunityFakeRepository();
+            var mockDataContext = MockRepository.GenerateMock<IUltimateContext>();
+            mockDataContext.Stub(x => x.Communities).Return((IDbSet<Community>)fakeRepo.All).Repeat.Once();
+            RepositoryFactory.Container.RegisterInstance(typeof(IUltimateContext), mockDataContext);
+
+            var uow = RepositoryFactory.Resolve<IUltimateUnitOfWork>();
+            var manager = new CommunityManager(uow);
+            Assert.AreEqual(3, manager.GetCommunities().Count);
+
+            mockDataContext.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void CommunityManager_GetAll_WithCache()
+        {
+            CacheManager.Clear(cacheKey);
+            CommunityFakeRepository fakeRepo = new CommunityFakeRepository();
+            var mockDataContext = MockRepository.GenerateMock<IUltimateContext>();
+            mockDataContext.Stub(x => x.Communities).Return((IDbSet<Community>)fakeRepo.All).Repeat.Once();
+            RepositoryFactory.Container.RegisterInstance(typeof(IUltimateContext), mockDataContext);
+
+            var uow = RepositoryFactory.Resolve<IUltimateUnitOfWork>();
+            var manager = new CommunityManager(uow);
+
+            Assert.AreEqual(3, manager.GetCommunities().Count);
+            Assert.IsNotNull(CacheManager.Get(cacheKey));
+            Assert.AreEqual(3, manager.GetCommunities().Count);
+            Assert.IsNotNull(CacheManager.Get(cacheKey));
+            Assert.AreEqual(3, manager.GetCommunities().Count);
+            Assert.IsNotNull(CacheManager.Get(cacheKey));
+
+            mockDataContext.VerifyAllExpectations();
+        }
+    }
+}
---
+++ Common/Tests/Services/CommunityServiceTest.cs
@@ -0,0 +1,55 @@
+﻿using Etsi.Ultimate.DataAccess;
+using Etsi.Ultimate.DomainClasses;
+using Etsi.Ultimate.Repositories;
+using Etsi.Ultimate.Services;
+using Etsi.Ultimate.Tests.FakeRepositories;
+using Etsi.Ultimate.Utils;
+using Microsoft.Practices.Unity;
+using NUnit.Framework;
+using Rhino.Mocks;
+using System.Data.Entity;
+
+namespace Etsi.Ultimate.Tests.Services
+{
+    [TestFixture]
+    public class CommunityServiceTest
+    {
+        private static string cacheKey = "ULT_COMMUNITY_MANAGER_ALL";
+
+        [Test]
+        public void CommunityService_GetAll()
+        {
+            CacheManager.Clear(cacheKey);
+            CommunityFakeRepository fakeRepo = new CommunityFakeRepository();
+            var mockDataContext = MockRepository.GenerateMock<IUltimateContext>();
+            mockDataContext.Stub(x => x.Communities).Return((IDbSet<Community>)fakeRepo.All).Repeat.Once();
+            RepositoryFactory.Container.RegisterInstance(typeof(IUltimateContext), mockDataContext);
+
+            var service = new CommunityService();
+            Assert.AreEqual(3, service.GetCommunities().Count);
+
+            mockDataContext.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void CommunityService_GetAll_WithCache()
+        {
+            CacheManager.Clear(cacheKey);
+            CommunityFakeRepository fakeRepo = new CommunityFakeRepository();
+            var mockDataContext = MockRepository.GenerateMock<IUltimateContext>();
+            mockDataContext.Stub(x => x.Communities).Return((IDbSet<Community>)fakeRepo.All).Repeat.Once();
+            RepositoryFactory.Container.RegisterInstance(typeof(IUltimateContext), mockDataContext);
+
+            var service = new CommunityService();
+
+            Assert.AreEqual(3, service.GetCommunities().Count);
+            Assert.IsNotNull(CacheManager.Get(cacheKey));
+            Assert.AreEqual(3, service.GetCommunities().Count);
+            Assert.IsNotNull(CacheManager.Get(cacheKey));
+            Assert.AreEqual(3, service.GetCommunities().Count);
+            Assert.IsNotNull(CacheManager.Get(cacheKey));
+
+            mockDataContext.VerifyAllExpectations();
+        }
+    }
+}
