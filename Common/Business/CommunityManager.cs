269a02c1d177ae00975beb1184dcb038088ac6e7
Capgemini <capgemini@none.com>
2014-04-18 14:09:45 +0000

    CommunityManager - Cache

--- Common/Business/CommunityManager.cs
+++ Common/Business/CommunityManager.cs
@@ -1,5 +1,6 @@
 ﻿using Etsi.Ultimate.DomainClasses;
 using Etsi.Ultimate.Repositories;
+using Etsi.Ultimate.Utils;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,6 +8,12 @@ namespace Etsi.Ultimate.Business
 {
     public class CommunityManager
     {
+        #region Constants
+
+        private static string CACHE_KEY = "ULT_COMMUNITY_MANAGER_ALL";
+
+        #endregion
+
         #region Properties
 
         private IUltimateUnitOfWork _uoW;
@@ -34,9 +41,17 @@ namespace Etsi.Ultimate.Business
         /// <returns>List of Communities</returns>
         public List<Community> GetCommunities()
         {
-            ICommunityRepository repo = RepositoryFactory.Resolve<ICommunityRepository>();
-            repo.UoW = _uoW;
-            return repo.All.ToList();
+            // Check in the cache
+            var cachedData = (List<Community>)CacheManager.Get(CACHE_KEY);
+            if (cachedData == null)
+            {
+                ICommunityRepository repo = RepositoryFactory.Resolve<ICommunityRepository>();
+                repo.UoW = _uoW;
+                cachedData = repo.All.ToList();
+
+                CacheManager.Insert(CACHE_KEY, cachedData);
+            }
+            return cachedData;
         }
 
         #endregion
