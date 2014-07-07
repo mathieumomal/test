84854f5f4bfca30277b690333e86af827eb46863
Capgemini <capgemini@none.com>
2014-07-07 12:50:38 +0000

    Implemented artf1501178 Create WCF Setup For offline mode.

---
+++ Offline/SyncServiceWindowsServiceSetup/SyncServiceInstaller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Configuration;
+using System.Configuration.Install;
+
+namespace SyncService
+{
+    [RunInstaller(true)]
+    public class SyncServiceInstaller: Installer
+    {
+        private ServiceProcessInstaller process;
+        private ServiceInstaller service;
+
+        public SyncServiceInstaller()
+        {
+            process = new ServiceProcessInstaller();
+            process.Account = ServiceAccount.LocalSystem;
+            service = new ServiceInstaller();
+            service.ServiceName = "3GU Synchronization Service";
+            Installers.Add(process);
+            Installers.Add(service);
+        }
+    }
+}
