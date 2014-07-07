9ee6dbcb3e4fd662b61ce2fd3fe5d0453a2b6dc4
Capgemini <capgemini@none.com>
2014-07-07 07:42:22 +0000

    Missing class.

--- Common/Business/WpmRecordCreator.cs
+++ Common/Business/WpmRecordCreator.cs
@@ -1,25 +1,57 @@
-﻿using System;
+﻿using Etsi.Ultimate.DomainClasses;
+using Etsi.Ultimate.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Etsi.Ultimate.DomainClasses;
-using Etsi.Ultimate.Repositories;
 
 namespace Etsi.Ultimate.Business
 {
-    class WpmRecordCreator
+    public class WpmRecordCreator
     {
-        IUltimateUnitOfWork UoW;
-
-        public WpmRecordCreator(IUltimateUnitOfWork iUoW)
+        public IUltimateUnitOfWork _uoW { get; set; }
+        
+        public WpmRecordCreator(IUltimateUnitOfWork Uow)
         {
-            UoW = iUoW;
+            _uoW = Uow;
         }
 
-        public int AddWpmRecords(SpecVersion iVersion)
+        /// <summary>
+        /// Add all needed records at WPMDB
+        /// </summary>
+        /// <param name="version">The transposed version</param>
+        /// <returns>ETSI work item identifier</returns>
+        public int AddWpmRecords(SpecVersion version) 
         {
+            IWorkProgramRepository wpRepo = RepositoryFactory.Resolve<IWorkProgramRepository>();
+            wpRepo.UoW = _uoW;
+
+            EtsiWorkItemImport importData = new EtsiWorkItemImport(version);
+
+            //Import Work Item to WPMDB
+            int WKI_ID = wpRepo.InsertEtsiWorkITem(importData);
+
+            //Import Schedule to WPMDB
+            wpRepo.InsertWIScheduleEntry(WKI_ID,0);
+
+            //Import Keyword to WPMDB
+            wpRepo.InsertWIKeyword(WKI_ID,"");
+
+            
+            //Get WPM project code 
+            IReleaseRepository releaseRepo = RepositoryFactory.Resolve<IReleaseRepository>();
+            releaseRepo.UoW = _uoW;
+            int releaseID = version.Fk_ReleaseId.GetValueOrDefault();
+            //Import project to WPMDB
+            wpRepo.InsertWIProject(WKI_ID, releaseRepo.Find(releaseID).WpmProjectId.GetValueOrDefault());
+
+            //Import Remark to WPMDB
+            wpRepo.InsertWIRemeark(WKI_ID,0,"");
+            
+
             return 0;
         }
-    }
+
+    }    
 }
