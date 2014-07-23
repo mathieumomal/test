cd3cba409c52f10214bf7471b1b7b665b33817b4
Capgemini <capgemini@none.com>
2014-07-23 15:55:24 +0000

    fo

---
+++ Common/DataAccess/UltimateContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.EntityClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etsi.Ultimate.DataAccess
+{
+    public partial class UltimateContext : DbContext, IUltimateContext
+    {
+        public UltimateContext(EntityConnection connection)
+            : base(connection, true)
+        {
+        }
+    }
+}
---
+++ Common/Tests/BaseEffortTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.EntityClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Effort.DataLoaders;
+using Etsi.Ultimate.DataAccess;
+using Etsi.Ultimate.Repositories;
+using NUnit.Framework;
+using Microsoft.Practices.Unity;
+
+namespace Etsi.Ultimate.Tests
+{
+    public abstract class BaseEffortTest
+    {
+        /// <summary>
+        /// Context
+        /// </summary>
+        public IUltimateContext Context { get; set; }
+
+        /// <summary>
+        /// UoW
+        /// </summary>
+        public IUltimateUnitOfWork UoW { get; set; }
+
+        /// <summary>
+        /// CSVs folder path
+        /// </summary>
+        public string CsvPath = "\\Data_For_EffortUnitTests\\";
+        /// <summary>
+        /// Context name (present in the App.config)
+        /// </summary>
+        public string ContextName = "UltimateContext";
+
+        /// <summary>
+        /// Initialization of the context by a set of csv files
+        /// </summary>
+        [SetUp]
+        public void init()
+        {
+            
+            //CSV files are stored in a specific folder : Data_For_EffortUnitTests
+            string dir = new StringBuilder()
+                .Append(Environment.CurrentDirectory)
+                .Append(CsvPath)
+                .ToString();
+            //Use Effort framework
+            IDataLoader loader = new Effort.DataLoaders.CsvDataLoader(dir);
+            try
+            {
+                EntityConnection connection = Effort.EntityConnectionFactory.CreateTransient(
+                    new StringBuilder().Append("name=").Append(ContextName).ToString(), 
+                    loader);
+                //Context creation
+                Context = new UltimateContext(connection);
+            }
+            catch (Exception e)
+            {
+                var error = new StringBuilder()
+                    .Append("An error occured when effort try to import CSVs files and/or load the context... Please check the next logs :")
+                    .Append(" MESSAGE : ")
+                    .Append(e.Message)
+                    .Append(" INNEREXCEPTION : ")
+                    .Append(e.InnerException)
+                    .ToString();
+                throw new Exception(error);
+            }
+
+            RepositoryFactory.Container.RegisterInstance(typeof(IUltimateContext), Context); 
+            UoW = RepositoryFactory.Resolve<IUltimateUnitOfWork>();
+        }
+
+
+        [TearDown]
+        public void end()
+        {
+            Context.Dispose();
+        }
+    }
+}
