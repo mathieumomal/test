bbb9b2350948f875c0246d2d6b7cebd2db2705b4
Capgemini <capgemini@none.com>
2014-09-29 14:34:23 +0000

    implemented Task artf1531146

---
+++ Common/Business/CrCategoriesManager.cs
@@ -0,0 +1,66 @@
+﻿using Etsi.Ultimate.DomainClasses;
+using Etsi.Ultimate.Repositories;
+using Etsi.Ultimate.Utils.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Etsi.Ultimate.Business
+{
+    public class CrCategoriesManager : ICrCategoriesManager
+    {
+
+        /// <summary>
+        /// Gets or sets the uoW.
+        /// </summary>
+        public IUltimateUnitOfWork UoW { get; set; }
+        private const string CacheKey = "ULT_BIZ_CHANGEREQUESTCATEGORY_ALL";
+        /// <summary>
+        /// Gets the change request categories.
+        /// </summary>
+        /// <param name="personId">The person identifier.</param>
+        /// <returns>CR Categories list </returns>
+        public List<Enum_CRCategory> GetChangeRequestCategories()
+        {
+            //cheeck cache data need to check User right
+            var cachedData = (List<Enum_CRCategory>)CacheManager.Get(CacheKey);
+            try
+            {
+                if (cachedData == null)
+                {
+                    var repo = RepositoryFactory.Resolve<IEnum_CrCategoryRepository>();
+                    repo.UoW = UoW;
+                    cachedData = repo.All.ToList();
+
+                    if (CacheManager.Get(CacheKey) == null)
+                    {
+                        CacheManager.Insert(CacheKey, cachedData);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogManager.Error("[Business] Failed to GetChangeRequestCategories:" + ex.Message);
+            }
+            return cachedData;
+        }
+    }
+
+    public interface ICrCategoriesManager
+    {
+        /// <summary>
+        /// Gets or sets the uoW.
+        /// </summary>
+        IUltimateUnitOfWork UoW { get; set; }
+
+        /// <summary>
+        /// Gets the change request categories.
+        /// </summary>
+        /// <param name="personId">The person identifier.</param>
+        /// <returns>CR Categories list</returns>
+        List<Enum_CRCategory> GetChangeRequestCategories();
+
+    }
+}
---
+++ Common/Services/CrCategoriesService.cs
@@ -0,0 +1,50 @@
+﻿using Etsi.Ultimate.Business;
+using Etsi.Ultimate.DomainClasses;
+using Etsi.Ultimate.Repositories;
+using Etsi.Ultimate.Utils.Core;
+using System;
+using System.Collections.Generic;
+
+namespace Etsi.Ultimate.Services
+{
+    public class CrCategoriesService
+    {
+        /// <summary>
+        /// Gets the change request categories.
+        /// </summary>
+        /// <param name="personId">The person identifier.</param>
+        /// <returns>
+        /// Change request Categories list</returns>
+        
+        public KeyValuePair<bool, List<Enum_CRCategory>> GetChangeRequestCategories()
+        {
+            List<Enum_CRCategory> enumChangeRequestCategorylist = new List<Enum_CRCategory>();
+            var isSuccess = true;
+            try
+            {
+                using (var uoW = RepositoryFactory.Resolve<IUltimateUnitOfWork>())
+                {
+                    var manager = ManagerFactory.Resolve<ICrCategoriesManager>();
+                    manager.UoW = uoW;
+                    enumChangeRequestCategorylist = manager.GetChangeRequestCategories();
+                }
+            }
+            catch (Exception ex)
+            {
+                LogManager.Error("[Service] Failed to GetChangeRequestCategories request: " + ex.Message);
+                isSuccess = false;
+            }
+            return new KeyValuePair<bool, List<Enum_CRCategory>>(isSuccess, enumChangeRequestCategorylist);
+        }
+    }
+
+    public interface ICrCategoriesService
+    {
+        /// <summary>
+        /// Gets the change request categories.
+        /// </summary>
+        /// <param name="personId">The person identifier.</param>
+        /// <returns>Change request Categories list</returns>
+        KeyValuePair<bool, List<Enum_CRCategory>> GetChangeRequestCategories();
+    }
+}
