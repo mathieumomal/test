ee1d9243d236e821b21aea393de94025bcf6e30c
Capgemini <capgemini@none.com>
2014-10-06 13:33:01 +0000

    Updated the database with an Enum_ChangeRequestStatus reference table, filled in by hand.

---
+++ Database/Scripts/Post-Deployment/Enum_ChangeRequestStatus.data.sql
@@ -0,0 +1,13 @@
+﻿TRUNCATE TABLE dbo.Enum_ChangeRequestStatus
+
+INSERT INTO dbo.Enum_ChangeRequestStatus(Pk_EnumChangeRequestStatus, Code, [Description]) VALUES
+(1, 'Agreed', 'Agreed'),
+(2, 'Approved','Approved'),
+(3, 'Noted','Noted'),
+(4, 'Postponed','Postponed'),
+(5, 'Rejected','Rejected'),
+(6, 'Revised','Revised'),
+(7, 'Merged','Merged'),
+(8, 'TechEndorsed','Technically endorsed'),
+(9, 'Withdrawn','Withdrawn'),
+(10, 'Reissued','Reissued')
---
+++ Database/dbo/Tables/Enum_ChangeRequestStatus.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Enum_ChangeRequestStatus] (
+    [Pk_EnumChangeRequestStatus] INT          NOT NULL,
+    [Code]                       VARCHAR (15) NULL,
+    [Description]                VARCHAR (50) NULL,
+    CONSTRAINT [PK_Enum_ChangeRequestStatus] PRIMARY KEY CLUSTERED ([Pk_EnumChangeRequestStatus] ASC)
+);
+
+
+
